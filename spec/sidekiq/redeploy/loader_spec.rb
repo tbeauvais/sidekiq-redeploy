46930d474aa580dbbdb001768a2471b6c0491c4f
Tom Beauvais <tbeauvais1@gmail.com>
2023-04-30 14:29:38 -0400

    Add archive loader from puma redeploy gem (#5)

--- spec/sidekiq/redeploy/loader_spec.rb
+++ spec/sidekiq/redeploy/loader_spec.rb
@@ -1,36 +1,43 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'puma-redeploy'
 
 RSpec.describe Sidekiq::Redeploy::Loader do
-  subject(:loader) { described_class.new(signal_mod: signal, process_mod: process, signal_delay: 0, loop_delay: 0) }
+  subject(:loader) { described_class.new(deployer:, logger:, config:) }
 
   let(:sidekiq_pid) { 456 }
   let(:loader_pid) { 123 }
-  let(:signal) { class_double(Signal, trap: nil) }
-  let(:process) { class_double(Process,  pid: loader_pid, getpgid: 0, kill: true) }
+  let(:config) { { signal_delay: 0, loop_delay: 0 } }
+  let(:deployer) do
+    instance_double(Puma::Redeploy::FileHandler, needs_redeploy?: false, deploy: 0, archive_file: 'test.zip',
+                                                 watch_file: 'ttt')
+  end
+  let(:logger) { instance_double(Logger, info: nil) }
 
   before do
+    allow(Signal).to receive(:trap)
+    allow(Process).to receive(:kill)
     allow(loader).to receive(:exit_loader).and_return(true)
     allow(loader).to receive(:process_died?).and_return(false)
-    allow(loader).to receive(:log)
   end
 
   describe '#run' do
     it 'adds trap for signals' do
-      allow(process).to receive(:fork).and_return(sidekiq_pid)
-      %w[INT TERM USR2 TTIN].each { |sig| expect(signal).to receive(:trap).with(sig) }
+      allow(Process).to receive(:fork).and_return(sidekiq_pid)
+      %w[INT TERM USR2 TTIN].each { |sig| expect(Signal).to receive(:trap).with(sig) }
       loader.run
     end
 
     it 'forks process' do
-      expect(process).to receive(:fork).and_return(sidekiq_pid)
+      expect(Process).to receive(:fork).and_return(sidekiq_pid)
       loader.run
     end
 
     context 'when sidekiq process has died' do
       it 'restarts sidekiq process' do
         allow(loader).to receive(:process_died?).and_return(true)
+        allow(Process).to receive(:kill)
         expect(loader).to receive(:fork_sidekiq).twice.and_return(sidekiq_pid)
         loader.run
       end
@@ -38,16 +45,16 @@ RSpec.describe Sidekiq::Redeploy::Loader do
 
     context 'when reloading sidekiq' do
       it 'sends quite and shutdown signals to sidekiq process' do
-        allow(process).to receive(:fork).and_return(sidekiq_pid)
+        allow(Process).to receive(:fork).and_return(sidekiq_pid)
         allow(loader).to receive(:reload_sidekiq).and_return(true)
-        expect(process).to receive(:kill).with('TSTP', sidekiq_pid)
-        expect(process).to receive(:kill).with('TERM', sidekiq_pid)
+        expect(Process).to receive(:kill).once.with('TSTP', sidekiq_pid)
+        expect(Process).to receive(:kill).twice.with('TERM', sidekiq_pid)
         loader.run
       end
 
       it 'forks sidekiq process' do
         allow(loader).to receive(:reload_sidekiq).and_return(true)
-        expect(process).to receive(:fork).twice.and_return(sidekiq_pid)
+        expect(Process).to receive(:fork).twice.and_return(sidekiq_pid)
         loader.run
       end
     end
